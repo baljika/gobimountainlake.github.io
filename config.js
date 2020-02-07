e8a2db0bb7686aad850e7cb06854291a782d9f22
Baljinnyam Enkhtur <33146728+baljika@users.noreply.github.com>
2020-02-07 19:43:36 +0800

    Initial Commit

---
+++ config.js
@@ -0,0 +1,224 @@
+var config = {
+    style: 'mapbox://styles/baligaa/ck6brf9co0d4z1ioeusqxdh23',
+    accessToken: 'pk.eyJ1IjoiYmFsaWdhYSIsImEiOiJjanQ1ZXpiOGIwM3BoM3ludmowbzdqN2Q0In0.hHPpqrclCIdqobui4RYT_w',
+    showMarkers: true,
+    alignment: 'left',
+    theme: 'dark',
+    title: 'SAND MOUNTAIN HORSE RIDING ADVENTURE',
+    subtitle: 'GREAT SAND DUNE LAKE | GREAT MONGOL ELS SAND DUNES NATIONAL PARK | SAND MOUNTAIN RIVER | KHANGAI MOUNTAINS | NOMAD FAMILY | ERDENE ZUU MONASTERY',
+    byline: '',
+    footer: 'Powered by: Gobi Mountain Lake LLC',
+    chapters: [
+        {
+            id: 'Day-1',
+            title: 'DAY 1: A DAY IN THE CAPITAL, ULAANBAATAR',
+            image: 'https://cdn.greensoft.mn/uploads/users/1492/images/GANDAN.jpg',
+            description: "'Your Mongolian adventure kicks off in the capital of Mongolia—Ulaanbaatar. With a rich Buddhist past and flourishing modern touches, Ulaanbaatar is the largest city in Mongolia and one of the fastest-growing in Asia. The culture, food, and attitude of its residents reflect this bustling spirit. Get settled in on your first day and check in to the centrally located hotel, your home for the next night. We take you through the main square where happened major events and celebrations during the socialist era, to the doorsteps of ancient Buddhist temples and the National History museum introducing Mongolia's history from early humans to the modern era. Let's then attend a Traditional Mongolian authentic performance including folk dances, throat singing, and contortion at the Tumen Ekh Ensemble. Welcoming dinner meet and greet will see you get to know your new travel family before we put you at total ease by introducing your Mongolia tour itinerary. So, you'll be more organized with the program and closer knowledge about your tour. Landing a few days before? No worries! Your airport pickup is included for any day before the tour and we can organize any extra nights required at the centrally located accommodation.'",
+            location: {
+                center: [106.91501, 47.93566],
+                zoom: 9.58,
+                pitch: 60.00,
+                bearing: 0
+              },
+              onChapterEnter: [
+                  {
+                      layer: 'Day-1',
+                      opacity: .25
+                  }
+              ],
+              onChapterExit: []
+          },
+          {
+            id: 'Day 2 DRIVING 90km',
+            title: 'DAY 2 driving 90km: FLIGHT TO ULIASTAI, GREAT MONGOL ELS SAND DUNES NATIONAL PARK',
+            image: '',
+            description: 'Transfer to the airport for the 2-hour flight to Uliastai town, is the capital of Zavkhan province, on the slopes of the Hangai Mountain. At the airport, your driver picks up us and venture to reach the Lodge of the Sand Mountain Expeditions. Here you will spend the next five nights in a Luxury Eco-Lodge, focused on preserving the environment and making a point to making everyone feel at home. It will be our starting point for the several expeditions in the region, uncovering the treasures of the West Gobi.',
+            location: {
+                center: [95.09916, 47.62467],
+                zoom: 6.19,
+                pitch: 60.0,
+                bearing: 0.0
+              },
+              onChapterEnter: [
+                {
+                  layer: 'Day-2',
+                  opacity: 0.25
+                }
+              ],
+              onChapterExit: []
+          },
+          {
+            id: 'DAY 3',
+            title: 'Day 3 RIDING 35km: A DAY IN THE GOBI MOUNTAIN LAKE  ',
+            image: 'https://cdn.greensoft.mn/uploads/users/1492/images/3_Fotor.jpg',
+            description: 'On the Khangai Mountain range Western plateau, the Great Sand Dune Lake ( Ulaagchini Black Lake )  is certainly considered as one of the jewels of the West Gobi. It is an amazingly beautiful freshwater lake, surrounded by sand mountains and on its northern shoreline by the Great Mongol Els sand dunes. Here, the dunes are ending themselves into the water, creating a surreal beach effect. This morning we meet our first Mongolian horses and local guides. For the next 4 days, you will likely ride the same horse every day, unless it’s not a good match or the horse needs a rest. Your guides will be pleased to assist you to adapt your riding style to suit the Mongolian horses and tack. You also discuss your saddles, stirrup lengths and other safety issues of horse riding before we begin our riding to the Great Sand Dune Lake. After 3 hours of wonderful riding through the gorgeous sand dune, valleys, up to the mountain, we will discover the great view of the surrounding area from the top of the southside mount of the lake. The view is amazing towards the endless sand dunes, mountains, lake, vast grasslands with many herds,  summer camps by nomads. A gourmet lakeside picnic will be set up for our lunch at the shore of the lake, facing lots of beautiful peninsulas and bays that penetrate into the lake water. We shall rest in the area for the afternoon, then ride back to the lodge.  In the evening, we will climb to the nearest hills. The view of the surrounding area from the top of the hill is amazing towards the endless Great Mongol Els sand dunes with the most beautiful sunset in the Gobi.',
+            location: {
+                center: [94.96487, 47.41337],
+                zoom: 12.54,
+                pitch: 45.00,
+                bearing: 7.56
+            },
+            onChapterEnter: [
+                {
+                    layer: 'DAY-3',
+                    opacity: 0.25
+                }
+            ],
+            onChapterExit: []
+        },
+        {
+            id: 'DAY 4',
+            title: 'Day 4 RIDING 20km: SAND MOUNTAIN EXPEDITION',
+            image: 'https://cdn.greensoft.mn/uploads/users/1492/images/CAMEL%20CARAVAN.jpg',
+            description: "'You'll begin your desert expedition by riding to the enormous sand mountain in the middle of the desert. The bulk of the pack camel caravan is behind you now, and you’ll follow your gobi desert guide to the luxury desert camp. There, the Mystery of the Sand Mountain camp will welcome us and preparing for you an outdoor exotic meal facing the beauty of the boundless Great Mongol Els sand dunes. The sheer vastness of the dunes is impressive, but we want to make sure you do more than simply stand in awe, so you'll set out on riding to traverse the sands through golden walled sand dunes, conquer nearby mountain to watch the sun sink below the horizon. After returning to camp, enjoy a traditional dinner—steamed dumplings with various kinds of salads under the stars. In the evening you are invited to a beautiful gobi bonfire cocktail party. Later you’ll drift to sleep in your very own luxurious turbo tent. '",
+            location: {
+              center: [95.03046, 47.38899],
+              zoom: 11.69,
+              pitch: 60.00,
+              bearing: 7.56
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'DAY 5',
+            title: 'Day 5 RIDING 30km: RIDE TO THE SAND MOUNTAIN RIVER',
+            image: 'https://cdn.greensoft.mn/uploads/users/1492/images/GML%20TOUR%20MUKHART.jpg',
+            description: "'Today's riding adventure takes you to a gigantic sand mountain. This is a unique opportunity to see a beautiful gobi river flows through the sands and creates an incredible gobi ecosystem, nourishing small bushes and trees giving wild fruits. Venture deeper into the sand mountain river, where the extremity of the environment is countered by the intense beauty of the rolling dunes and flowing river. After 2 hours awesome ride across the Great Mongol Els Sand Dunes where we can trot or canter your horse, we will explore the breathtaking oasis of the West Gobi, the Sand Dune River. Walking barefoot towards the starting point is a little piece of paradise where the river flows under the tall sand mountain. Enjoy a delicious picnic then start great ride to the lodge through the another side of the river.'",
+            location: {
+                center: [94.84406, 47.42882],
+                zoom: 11.67,
+                pitch: 45.00,
+                bearing: 0.0
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'DAY 6',
+            title: 'Day 6 RIDING 30km: EXPLORE THE GATE ROCK',
+            image: 'https://cdn.greensoft.mn/uploads/users/541/images/1_SENJIT_Fotor.jpg',
+            description: 'The final segment of your sand mountain horse riding adventure takes you to the mountainside. This is an amazing opportunity to see the whole view of the Great Mongol Els sand dunes national park from the top of the mountain. After 2 hours awesome ride up to the Tovkhosh mountain, we will explore the breathtaking view bothe of the sand dunes and mountains, endless grasslands. Explore the Gate Rock, unique rocky formation on the summit of the mountain and enjoy a delicious high side picnic then start another great ride to the lodge. Soak up all the wonders on this relaxing, picturesque loop ride up to the mountains and down back to lodge with a view of the great sand ocean. Within arrival at the lodge, we will say goodbye to your horses and horse guides.',
+            location: {
+                center: [94.84266, 47.44601],
+                zoom: 12.57,
+                pitch: 45.00,
+                bearing: 0.0
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'DAY 7',
+            title: 'Day 7 DRIVING 240km: TRANQUIL LAKES AND THE HOLY MOUNTAINS',
+            image: 'https://cdn.greensoft.mn/uploads/users/1492/images/8_Fotor.jpg',
+            description: 'We will leave the West Gobi to venture towards the mountain forest zone and reach the Green Gem Lake of the Khangai Mountain chain, which is located in the Ider Village of Zavkhan Province. Drive through stunning views made of mountains, forests, and nomadic campsites. The Khangai mountain chain is one of the three largest mountain ranges in the country. The sacred peak, Mount OtgonTenger (3905m), towering at 4021 meters above sea level, has worshipped for generations by Mongols. The tradition of worshipping sacred mountains and waters is one of the outstanding cultural heritage elements created, developed and practiced by generations to generations since the Hunnu Empire. Including the lake and surrounding with the holy Mount is protected as the Strictly Protected Area. Stay overnight under a ger in a small camp at the shore of the Green Gem Lake.',
+            location: {
+                center: [97.65189, 48.00664],
+                zoom: 9.03,
+                pitch: 60.00,
+                bearing: 10.4
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'DAY 8',
+            title: 'Days 8 - 9: EXPLORE THE WATERFALL',
+            image: 'https://cdn.greensoft.mn/uploads/users/1492/images/1_TARVAGATAI.jpg',
+            description: "Day 8.  <br/> The landscape completely changes here since we entered in the high mountain, forest zone. In the morning, we meet again our Mongolian horses and local guides. A smooth warm-up ride today, about 3 hours mostly at a walk and easy trot up to the valley to explore the Tahilt waterfall and its surroundings. Our ride will take a place through the mountain trails and forests of larches and pines, wild roses and wildflowers, junipers, gooseberries, and wild onions. This rich alpine-styled vegetation that will delight botany lovers. We will take a break at the high located waterfall for a picnic lunch with a great view of the valley. After lunch, we will return to our previous camp, again traversing beautiful mountain trails. <br/> Riding     20 km -  4 hours <br/> Day 9. <br/> Continue the breathtaking horse riding adventure up to the river in the mountainside. Enjoy the panoramic view of the valley. At the altitude of 2,400 meters above sea level, the climate here is humid and chilly where the deers and much other wildlife thrive. First, we ride up to the valley, then we will ride across plains and forests to reach Deer jumping gorge, the stunning narrow canyon of the park ( 2meters wide, 20 meters deep). Have a mountainside picnic and delight you with the wilderness of the mountainside. On occasion, we will dismount to lead our horses down the steep paths when riding down.  Spend a pleasant day riding in is a simple yet beautiful area. In the evening we say thank and goodbye to our local horse guides and lovely horses. Stay overnight at the ger camp Riding   <br/>  30 km - 5 hours",
+            location: {
+                center: [97.66065, 48.04333],
+                zoom: 12.04,
+                pitch: 60.00,
+                bearing: 10.4
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'DAY 10',
+            title: 'Day 10 DRIVING 280km: AMONG NOMADIC YAK HERDING FAMILY',
+            image: 'https://cdn.greensoft.mn/uploads/users/1492/images/GML%20TOUR%20PHOTOS.jpg',
+            description: 'We will head to a nomad family in heart of the Khangai Mountain Range, the central part of Mongolia. The area has many historical vestiges, the picturesque landscapes, surrounded by verdant Khangai Range, the rich fauna and flora, make this area the perfect place to discover the nomadic lifestyle of Mongolia. We will pass two high passes on the way, driving through stunning views made of mountains, forests, and nomadic campsites. In the next three days, we’ll stay with a nomad family where you experience nomadic lifestyle closer, socialize with nomads, play with children and attend daily duties such as milking a yak and producing dairy products. In this region, we also cooperate with our nomad family for the Argadai and Berhdei Projects which breed the native Mongolian guardian dog Bankhar. Say hello to our dogs.',
+            location: {
+                center: [99.96456, 48.14874],
+                zoom: 8.24,
+                pitch: 60.00,
+                bearing: 10.40
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'DAY 11',
+            title: 'Day 11 RIDING 20km: RIDE TO THE LOWER VALLEY AND LAKES',
+            image: 'https://cdn.greensoft.mn/uploads/users/1492/images/HORSE_Fotor.jpg',
+            description: 'This morning we meet our next team of Mongolian horses and local horse guides for the next 2 days. These days, we ride across the sacred valleys of Khangai massif where we will do a great ride across the mountains and up to the high side lakes. This wonderful valley contrasts with the previous landscapes of the mountain, with beautiful flora. Also, we learn more about the everyday life of nomads and gain knowledge about the horse culture in Mongolia with welcome guidance by the herders during our stay. Our nomad family owned horses, cows, goats, and sheep. At this altitude (about 2500meters), the cows are replaced with yaks and hybrids by nomad families.  First, we will ride down valley and across plains and reach Lake. This wild lake is lined with forests. Enjoy a lakeside picnic and cool off your feet in its cold waters that delight you. After lunch, we will return to our previous camp, again traversing stunning mountain trails.',
+            location: {
+                center: [100.48985, 48.40453],
+                zoom: 9.28,
+                pitch: 60.00,
+                bearing: 10.4
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'DAY 12',
+            title: 'Day 12 RIDING 20km: HOME OF THE WILD HORSE',
+            image: 'https://cdn.greensoft.mn/uploads/users/1492/images/horse.jpg',
+            description: "Day twelveth marks the last day of riding, you'll start a ride little far from the nomad family to help our herders for bringing their horses to camp. Nomads keep Mongolian horses free with their own happiness. The horses graze freely on the grassland in the distance approx 20 km around the summer camp of the family, and it will be a great opportunity for us to observe the Mongolian horse's behaviors. Possibly we can join young nomad boys of our host family when they herd the cows, goats, and sheep. It is amazing to see young boys sometimes riding horses without saddles. In the evening, our nomads welcome us with Mongolian barbecue, khorkhog as a true traditional meal for dinner.",
+            location: {
+                center: [100.46949, 48.53551],
+                zoom: 9.28,
+                pitch: 60.0,
+                bearing: 10.40
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'DAY 13',
+            title: 'Day 13 DRIVING 380km: THE ANCIENT CAPITAL OF THE MONGOLS, KHARKHORIN',
+            image: 'https://cdn.greensoft.mn/uploads/users/1492/images/Erdenezuu.jpg',
+            description: 'Today we will travel to Kharkhorin, the ancient capital of the Great Mongol Empire. The legendary city was founded by Genghis Khan in 1220. There we will visit one of the oldest and most beautiful monasteries: Erdene Zuu, built by Abtai Sain Khan in 1585. Shortly after we will immerse ourselves into the deep history of the Great Empire at the city History museum. On the way, we will take a pleasant stop at the Chuluut Canyon which is situated by the earthquake and the beautiful White Lake. Have another nice stop for a picnic lunch. In the afternoon, we will welcome you to our apartment hotel, Kharkhorin Expedition.',
+            location: {
+                center: [102.82227, 47.19577],
+                zoom: 13.25,
+                pitch: 60.0,
+                bearing: 10.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'DAY 14',
+            title: 'Day 14 DRIVING 360km: JOURNEY BACK TO ULAANBAATAR',
+            image: 'https://cdn.greensoft.mn/uploads/users/1492/images/GML%20TOUR%20UB%20ITY%20PHOTO.jpg',
+            description: 'Bid Central Mongolia goodbye this morning. Drive back to the city, possibly visit the factory store of cashmere and transfer to your hotel. With your guide, you can go and shop in the center of the city. Have a farewell dinner at a restaurant.',
+            location: {
+                center: [106.91676, 47.91873],
+                zoom: 11.74,
+                pitch: 60.00,
+                bearing: 0.0
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+         },
+         {
+             id: 'DAY 15',
+             title: 'Day 15: FLIGHT RETURN TO HOME',
+             image: '',
+             description: 'The final day, depart home after spending a fulfilling and active two weeks and exploring some of Mongolia’s best! Our crew will drive you to the airport for an international flight',
+             location: {
+               center: [106.91676, 47.91873],
+               zoom: 11.74,
+               pitch: 60.00,
+               bearing: 0.0
+             },
+             onChapterEnter: [],
+             onChapterExit: []
+         },
+    ]
+};
